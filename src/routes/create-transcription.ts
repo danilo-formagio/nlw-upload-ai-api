160dc2bb9269bdf13fb8ae7e918d368cd8a040b6
Danilo Formagio <daniloformagio@gmail.com>
2023-09-13 23:13:39 -0300

    OpenAI API integration and new routes for create transcription and generate ai completion

--- src/routes/create-transcription.ts
+++ src/routes/create-transcription.ts
@@ -2,6 +2,7 @@ import { FastifyInstance } from "fastify";
 import { z } from 'zod';
 import { prisma } from "../lib/prisma";
 import { createReadStream } from "node:fs";
+import { openai } from "../lib/openai";
 
 export async function createTranscriptionRoute(app: FastifyInstance) {
   app.post('/videos/:videoId/transcription', async (request) => {
@@ -25,11 +26,26 @@ export async function createTranscriptionRoute(app: FastifyInstance) {
 
     const videoPath = video.path;
     const audioReadStream = createReadStream(videoPath);
+    const response = await openai.audio.transcriptions.create({
+      file: audioReadStream,
+      model: 'whisper-1',
+      language: 'pt',
+      response_format: 'json',
+      temperature: 0,
+      prompt
+    });
 
-    return {
-      videoId,
-      prompt,
-      videoPath
-    }
+    const transcription =  response.text;
+
+    await prisma.video.update({
+      where: {
+        id: videoId,
+      },
+      data: {
+        transcription
+      }
+    });
+
+    return { transcription };
   });
 }
