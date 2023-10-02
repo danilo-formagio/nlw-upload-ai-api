d9ee96f6b6850576dffcd43b12058416cacb0d6d
Danilo Formagio <daniloformagio@gmail.com>
2023-10-02 11:33:48 -0300

    Create new endpoint to download video from Youtube URL using ytdl-core lib

---
+++ src/routes/download-youtube-video.ts
@@ -0,0 +1,41 @@
+import { FastifyInstance } from "fastify";
+import ytdl from "ytdl-core";
+import { z } from "zod";
+import fs from "node:fs";
+import path from "node:path";
+
+export async function downloadYoutubeVideoRoute(
+  app: FastifyInstance
+): Promise<void> {
+  app.get("/download/:videoId", async (request, reply) => {
+    const bodySchema = z.object({
+      videoId: z.string(),
+    });
+
+    const { videoId } = bodySchema.parse(request.params);
+    const video = await downloadVideo(videoId);
+
+    reply.send(video);
+  });
+}
+
+function downloadVideo(videoId: string): Promise<Buffer> {
+  const videoURL = `https://www.youtube.com/watch?v=${videoId}`;
+  const videoPathDestination = path.resolve(
+    __dirname,
+    "../../tmp",
+    "video.mp4"
+  );
+
+  return new Promise<Buffer>((resolve, reject) => {
+    ytdl(videoURL, { quality: "lowestaudio", filter: "audioonly" })
+      .on("end", () => {
+        const stream = fs.readFileSync(videoPathDestination);
+        resolve(stream);
+      })
+      .on("error", (error) => {
+        reject(error);
+      })
+      .pipe(fs.createWriteStream(videoPathDestination));
+  });
+}
--- src/server.ts
+++ src/server.ts
@@ -4,6 +4,7 @@ import { getAllPromptsRoute } from "./routes/get-all-prompts";
 import { uploadVideoRoute } from "./routes/upload-video";
 import { createTranscriptionRoute } from "./routes/create-transcription";
 import { generateAICompletionRoute } from "./routes/generate-ai-completion";
+import { downloadYoutubeVideoRoute } from "./routes/download-youtube-video";
 
 const app = fastify();
 
@@ -15,6 +16,7 @@ app.register(getAllPromptsRoute);
 app.register(uploadVideoRoute);
 app.register(createTranscriptionRoute);
 app.register(generateAICompletionRoute);
+app.register(downloadYoutubeVideoRoute);
 
 app.listen({
   port: 3333
