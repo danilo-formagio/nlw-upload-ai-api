5ed6036362118002badc394276ccd1755a14d642
Danilo Formagio <daniloformagio@gmail.com>
2023-10-02 12:17:40 -0300

    Apply prettier formatter

--- src/routes/download-youtube-video.ts
+++ src/routes/download-youtube-video.ts
@@ -1,15 +1,13 @@
-import { FastifyInstance } from "fastify";
-import ytdl from "ytdl-core";
-import { z } from "zod";
-import fs from "node:fs";
-import path from "node:path";
+import { FastifyInstance } from 'fastify';
+import ytdl from 'ytdl-core';
+import { z } from 'zod';
+import fs from 'node:fs';
+import path from 'node:path';
 
-export async function downloadYoutubeVideoRoute(
-  app: FastifyInstance,
-): Promise<void> {
-  app.get("/download/:videoId", async (request, reply) => {
+export async function downloadYoutubeVideoRoute(app: FastifyInstance): Promise<void> {
+  app.get('/download/:videoId', async (request, reply) => {
     const bodySchema = z.object({
-      videoId: z.string(),
+      videoId: z.string()
     });
 
     const { videoId } = bodySchema.parse(request.params);
@@ -21,19 +19,15 @@ export async function downloadYoutubeVideoRoute(
 
 function downloadVideo(videoId: string): Promise<Buffer> {
   const videoURL = `https://www.youtube.com/watch?v=${videoId}`;
-  const videoPathDestination = path.resolve(
-    __dirname,
-    "../../tmp",
-    "video.mp4",
-  );
+  const videoPathDestination = path.resolve(__dirname, '../../tmp', 'video.mp4');
 
   return new Promise<Buffer>((resolve, reject) => {
-    ytdl(videoURL, { quality: "lowestaudio", filter: "audioonly" })
-      .on("end", () => {
+    ytdl(videoURL, { quality: 'lowestaudio', filter: 'audioonly' })
+      .on('end', () => {
         const stream = fs.readFileSync(videoPathDestination);
         resolve(stream);
       })
-      .on("error", (error) => {
+      .on('error', error => {
         reject(error);
       })
       .pipe(fs.createWriteStream(videoPathDestination));
--- src/server.ts
+++ src/server.ts
@@ -1,15 +1,15 @@
-import { fastify } from "fastify";
-import { fastifyCors } from "@fastify/cors";
-import { getAllPromptsRoute } from "./routes/get-all-prompts";
-import { uploadVideoRoute } from "./routes/upload-video";
-import { createTranscriptionRoute } from "./routes/create-transcription";
-import { generateAICompletionRoute } from "./routes/generate-ai-completion";
-import { downloadYoutubeVideoRoute } from "./routes/download-youtube-video";
+import { fastify } from 'fastify';
+import { fastifyCors } from '@fastify/cors';
+import { getAllPromptsRoute } from './routes/get-all-prompts';
+import { uploadVideoRoute } from './routes/upload-video';
+import { createTranscriptionRoute } from './routes/create-transcription';
+import { generateAICompletionRoute } from './routes/generate-ai-completion';
+import { downloadYoutubeVideoRoute } from './routes/download-youtube-video';
 
 const app = fastify();
 
 app.register(fastifyCors, {
-  origin: "*",
+  origin: '*'
 });
 
 app.register(getAllPromptsRoute);
@@ -19,5 +19,5 @@ app.register(generateAICompletionRoute);
 app.register(downloadYoutubeVideoRoute);
 
 app.listen({ port: 3333 }).then(() => {
-  console.log("HTTP Server Running");
+  console.log('HTTP Server Running');
 });
